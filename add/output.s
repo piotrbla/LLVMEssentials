9e05602697e497d9c16a4e6a0cb1f5b5edc1facc
PiotrB <piotrb@gmail.com>
2022-06-09 16:08:58 +0200

    Generated files

---
+++ add/output.s
@@ -0,0 +1,69 @@
+	.text
+	.file	"llvm-link"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -8(%rbp)
+	movl	$2, %edi
+	callq	add
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %esi
+	movabsq	$.L.str, %rdi
+	movb	$0, %al
+	callq	printf
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.globl	add                     # -- Begin function add
+	.p2align	4, 0x90
+	.type	add,@function
+add:                                    # @add
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	globvar, %eax
+	addl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end1:
+	.size	add, .Lfunc_end1-add
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%d\n"
+	.size	.L.str, 4
+
+	.type	globvar,@object         # @globvar
+	.data
+	.globl	globvar
+	.p2align	2
+globvar:
+	.long	12                      # 0xc
+	.size	globvar, 4
+
+	.ident	"clang version 10.0.0-4ubuntu1 "
+	.ident	"clang version 10.0.0-4ubuntu1 "
+	.section	".note.GNU-stack","",@progbits
